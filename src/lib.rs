f60ce2c93ce4c8277be42827e8f6c90a7c66d2e5
Christoph Rohland <hcrohland@users.noreply.github.com>
2019-05-02 14:53:15 +0200

    Add logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
+#[macro_use] extern crate log;
+
 use std::env;
+
+use simplelog::{
+    CombinedLogger,
+    LevelFilter,
+    TermLogger,
+    WriteLogger,
+};
+use std::fs::File;
 
 pub struct Config {
     pub greeting: String,
@@ -14,4 +24,22 @@ impl Default for Config{
             greeting: greet,
         }
     }
+}
+
+pub fn init_logging (){
+    const LOGFILE_NAME: &'static str = "tendabike.log";
+    CombinedLogger::init(vec![
+        TermLogger::new(LevelFilter::Info, simplelog::Config::default()).expect("Couldn't get terminal logger"),
+        WriteLogger::new(
+            LevelFilter::Debug,
+            simplelog::Config::default(),
+            File::create(LOGFILE_NAME).expect("Couldn't create logfile"),
+        ),
+    ])
+    .expect("Can't get logger.");
+
+    trace!("Trace");
+    debug!("Debug");
+    info!("Info");
+    error!("Error");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+extern crate simplelog;
+
 extern crate dotenv;
 
 extern crate tendabike;
@@ -8,15 +10,17 @@ extern crate tendabike;
 use rocket::State;
 use tendabike::Config;
 
+
 #[get("/")]
 fn index(conf: State<Config>) -> String {
     conf.greeting.clone()
 }
 
 fn main() {
-
 // read environment variables from file
     dotenv::dotenv().ok();
+
+    tendabike::init_logging();
 
     rocket::ignite()
         .manage(Config::default())
