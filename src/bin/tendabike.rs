400ea78ca8832521e681bcdf7caf7dd8915b487d
Christoph Rohland <hcrohland@users.noreply.github.com>
2019-05-14 21:46:47 +0200

    Cleanup tendabike.rs

--- src/bin/tendabike.rs
+++ src/bin/tendabike.rs
@@ -1,22 +1,12 @@
 
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use] extern crate rocket;
-
+extern crate rocket;
 extern crate tendabike;
-
 use tendabike::*;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, want to tend your bikes?"
-}
-
 fn main() {
 
     // setup environment. Includes Config and logging
     init_environment();
 
     // start the server
-    tendabike::ignite_rocket().launch();
+    ignite_rocket().launch();
 }
