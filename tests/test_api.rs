e207286631b09cf0e3549593ff0ae61e44c73797
Christoph Rohland <hcrohland@users.noreply.github.com>
2019-05-15 15:33:00 +0200

    What the hell are "Higher Rank Trait Bounds"?

--- tests/test_api.rs
+++ tests/test_api.rs
@@ -6,40 +6,23 @@
 
     use tendabike::part::*;
 
-    fn myreq<'c, 'u: 'c, U: Into<std::borrow::Cow<'u, str>>> (client: &'c Client, uri: U) -> LocalResponse<'c> {
-        let response = client.get(uri).header(Header::new("x-user-id", "2")).dispatch();
+    use serde::de::Deserialize;
+    fn getjson<'c, 'u, T, U> (client: &'c Client, uri: U) -> T 
+        where for<'a> T: Deserialize<'a>, U: Into<std::borrow::Cow<'u, str>>,
+    {
+        let mut response = client.get(uri).header(Header::new("x-user-id", "2")).dispatch();
         assert_eq!(response.status(), Status::Ok);
-        response
-    }
 
-    fn getparts<'a, 'b> (client: &'a Client, uri: &'b str) -> Vec<Part> {
-        let mut response = myreq (client, uri);
-
-        let body = response.body_string().expect("body is no string");
-        let myparts = serde_json::from_str(
-                &body
-            ).expect("malformed body");
-        myparts
-    }
-
-    /* use serde::de::Deserialize;
-    fn getjson<'c, 'u, T: Deserialize<'c>> (client: &'c Client, uri: &'u str) -> T {
-        let mut response = myreq (client, uri);
-
-        let body = response.body_string().expect("body is no string");
-        let myparts: T = serde_json::from_str(
-                &body
-            ).expect("malformed body");
-        myparts
-    } */ 
+        serde_json::from_str::<T>(
+                &response.body_string().expect("body is no string")
+            ).expect("malformed body")
+    } 
 
     #[test]
     fn part_types () {
             let client = Client::new(crate::ignite_rocket()).expect("valid rocket instance");
 
-            let mut response = myreq (&client, "/part/types");
-            let types: Vec<PartTypes> = serde_json::from_str(&response.body_string().expect("")).expect("");
-            assert_eq!(types.len(), 9);
+            let types: Vec<PartTypes> = getjson (&client, "/part/types");
             assert_eq!(types[0], PartTypes{id:1,name: String::from("Bike"), main:true, hooks: vec!(2,4,5,7,8)});
     }
     #[test]
@@ -49,17 +32,13 @@
         let response = client.get("/part/999").header(Header::new("x-user-id", "2")).dispatch();
         assert_eq!(response.status(), Status::NotFound);
 
-        let _myspares: Vec<Part> = getparts(&client, "/part/myspares");
-        let myparts: Vec<Part> = getparts(&client, "/part/mygear");
+        let _myspares: Vec<Part> = getjson(&client, "/part/myspares");
+        let myparts: Vec<Part> = getjson(&client, "/part/mygear");
 
-        let mut response = myreq(&client, format!("/part/{}", myparts[0].id));
-        let part: Part = serde_json::from_str(&response.body_string()
-            .expect("body is no string")).expect("body is no part");
+        let part: Part = getjson(&client, format!("/part/{}", myparts[0].id));
         assert_eq!(part.name.to_string(), "Bronson");
 
-        let mut response = myreq (&client, format!("/part/{}?assembly", myparts[1].id));
-        let ass: Assembly = serde_json::from_str(&response.body_string()
-            .expect("body is no string")).expect("body is no assembly");
+        let ass: Assembly = getjson (&client, format!("/part/{}?assembly", myparts[1].id));
         assert_eq!(ass.part.name.to_string(), "Slide");
     }
 
@@ -69,9 +48,7 @@
     fn activity_types () {
             let client = Client::new(crate::ignite_rocket()).expect("valid rocket instance");
 
-            let mut response = myreq(&client, "/activ/types");
-            let types: Vec<ActivityType> = serde_json::from_str(&response.body_string().expect("")).expect("");
-            assert!(types.len() > 0);
+            let types: Vec<ActivityType> = getjson(&client, "/activ/types");
             assert_eq!(types[0], ActivityType {id:1,name: String::from("Bike Ride"), gear: 1});
     }
     #[test]
@@ -81,10 +58,7 @@
         let response = client.get("/activ/999").header(Header::new("x-user-id", "2")).dispatch();
         assert_eq!(response.status(), Status::NotFound);
 
-        let mut response = myreq(&client, "/activ/1");
-        let _part: Activity = serde_json::from_str(&response.body_string()
-            .expect("body is no string")).expect("body is no activity");
-
+        let _part: Activity = getjson(&client, "/activ/1");
     }
 /* 
     #[test]
