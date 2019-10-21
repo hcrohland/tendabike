0a7fc58ab1cdbd99225d6a8e42f9c6ac7c62b706
Christoph Rohland <hcrohland@users.noreply.github.com>
2019-10-21 22:15:38 +0200

    Flexible sync batches

--- tb_strava/src/tb.rs
+++ tb_strava/src/tb.rs
@@ -2,22 +2,22 @@ use crate::*;
 use auth::User;
 use activity::*;
 
-fn next_activities(user: &User) -> TbResult<Vec<StravaActivity>> {
+fn next_activities(user: &User, per_page: Option<i32>) -> TbResult<Vec<StravaActivity>> {
 
-    let r = user.request(&format!("/activities?after={}&per_page=10", user.last_activity()))?;
+    let r = user.request(&format!("/activities?after={}&per_page={}", user.last_activity(), per_page.unwrap_or(10)))?;
     // let r = user.request("/activities?per_page=2")?;
     let acts: Vec<StravaActivity> = serde_json::from_str(&r)?;
     Ok(acts)
 }
 
-#[get("/next")]
-fn next (user: User) -> ApiResult<Vec<TbActivity>> {
-    tbapi(next_activities(&user)?.into_iter().map(|a| a.into_tb(&user)).collect())
+#[get("/next?<batch>")]
+fn next (batch: Option<i32>, user: User) -> ApiResult<Vec<TbActivity>> {
+    tbapi(next_activities(&user, batch)?.into_iter().map(|a| a.into_tb(&user)).collect())
 }
 
-#[get("/sync")]
-fn sync (user: User) -> ApiResult<Vec<serde_json::Value>> {
-    let acts = next_activities(&user)?;
+#[get("/sync?<batch>")]
+fn sync (batch: Option<i32>, user: User) -> ApiResult<Vec<serde_json::Value>> {
+    let acts = next_activities(&user, batch)?;
    
     tbapi(acts.into_iter()
         .map(|a| a.send_to_tb(&user))
