d18bdc4505ac964846a7e1837c02f061914f72b6
Christoph Rohland <hcrohland@users.noreply.github.com>
2019-11-23 21:10:49 +0100

    Beautify category selection

--- tb_svelte/src/Home.svelte
+++ tb_svelte/src/Home.svelte
@@ -7,12 +7,20 @@ $category = null;
 
 {#await promise}
 	loading
-{:then data}
-	{#each data as cat}
-		 <li>
-		 <a href="#/{cat}"> {$types[cat].name} </a>
-		 </li>
-	{/each}
+{:then categories}
+	<div class="row m-0 p-0">
+		{#each categories as cat}
+			<div class="col-md-6 p-0 p-sm-2">
+				<div class="card">
+					<div class="card-header">
+						<h5 class="mb-0"> 
+							<a class="card-link stretched-link text-dark" href="#/{cat}"> {$types[cat].name}s </a>
+						</h5>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
 {:catch error}
   {error}
 {/await}
