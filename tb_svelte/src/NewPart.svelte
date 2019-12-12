6349112cb02587d33bf40a17a03b0a9ded624a9b
Christoph Rohland <hcrohland@users.noreply.github.com>
2019-12-12 18:36:56 +0100

    NewPart UI step 1

---
+++ tb_svelte/src/NewPart.svelte
@@ -0,0 +1,59 @@
+<script>
+  import Await from './Await.svelte';
+  import Modal from './Modal.svelte';
+  import {myfetch, filterValues, types, parts, category} from './store.js';
+    
+  let name;
+  let vendor;
+  let model;
+  let purchase;
+
+  function savePart () {
+    if (name) {
+      let part = {id: 999, 
+        what: $category.id, 
+        count:0, climb:0, descend:0, distance:0, time: 0,
+        name, vendor, model, purchase};
+      parts.updateMap([part])
+    }
+  }
+
+</script>
+
+<Modal title='New {$category.name}' submit={savePart} save="Create">
+
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-12">
+      <label for="inputName">You call your new {$category.name}</label>
+      <input type="text" class="form-control" id="inputName" bind:value={name} required placeholder="Name">
+    </div>
+  </div>
+  <div class="form-row">
+
+  <div class="form-group col-md-6">
+    <label for="inputBrand">and it is a</label>
+    <input type="text" class="form-control" id="inputBrand" bind:value={vendor} placeholder="Brand">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="inputModel"> &nbsp </label>
+    <input type="text" class="form-control" id="inputModel" bind:value={model} placeholder="Model">
+  </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputDate">New {$category.name} day was at</label>
+      <input type="date" class="form-control" id="inputDate" bind:value={purchase} required>
+    </div>
+  </div>
+  <!-- <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div> -->
+  <!-- <button type="submit" class="btn btn-primary">Create</button> -->
+</form>
+</Modal>
--- tb_svelte/src/ToyGroup.svelte
+++ tb_svelte/src/ToyGroup.svelte
@@ -2,6 +2,7 @@
   import { onDestroy } from 'svelte';
   import Gear from './Gear.svelte';
   import Await from './Await.svelte';
+  import NewPart from './NewPart.svelte';
   import {myfetch, filterValues, types, parts, category} from './store.js';
   
   export let params;
@@ -9,20 +10,24 @@
   category.set($types[params.category]);
   onDestroy(() => category.set(undefined));
   
-  const gears = () => filterValues($parts, (p) => p.what === $category.id)
+  const gears = () => filterValues($parts, (p) => p.what == params.category)
 </script>
 
 {#if $category }
   <div class="row border p-sm-2">
-    {#each gears() as {id} (id)}
+    {#each gears() as part (part.id)}
       <div class="col-md-6 p-0 p-sm-2">
-        <Gear part={$parts[id]} />
+        <Gear {part} />
       </div>
     {:else}
       You have no {$category.name} to tend 😱
     {/each}
   </div>
-
+  <div class="container">
+      <div class="row p-sm-2">
+        <NewPart />
+      </div>
+  </div>
 {:else}
      Error: Category not found!
 {/if}
