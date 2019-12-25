350203fd6b6e204aa817b781c4afe39551270a2f
Christoph Rohland <hcrohland@users.noreply.github.com>
2019-12-25 19:19:03 +0100

    Only scroll the table content

--- tb_svelte/src/Part.svelte
+++ tb_svelte/src/Part.svelte
@@ -12,24 +12,33 @@
   promise = myfetch('/attach/to/' + gear.id) 
 </script>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">{$types[gear.what].name}</th>
-      <th scope="col">Brand</th>
-      <th scope="col">Model</th>
-      <Usage />
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>{gear.name}</td>
-      <td>{gear.vendor}</td>
-      <td>{gear.model}</td>
-      <Usage part_id={gear.id} />
-    </tr>
-  </tbody>
-</table>
-<Await {promise} let:data={subparts}>
-  <Subparts hook={gear.what} {subparts} />
-</Await>
+<style>
+.scroll-x {
+  width: 100%;
+  overflow-x: scroll;
+}
+</style>
+
+<div class="scroll-x">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">{$types[gear.what].name}</th>
+        <th scope="col">Brand</th>
+        <th scope="col">Model</th>
+        <Usage />
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{gear.name}</td>
+        <td>{gear.vendor}</td>
+        <td>{gear.model}</td>
+        <Usage part_id={gear.id} />
+      </tr>
+    </tbody>
+  </table>
+  <Await {promise} let:data={subparts}>
+    <Subparts hook={gear.what} {subparts} />
+  </Await>
+</div>
