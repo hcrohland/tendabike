d3c95f7c7b2f8923028de2dc88bfd0c897e5219d
Christoph Rohland <hcrohland@users.noreply.github.com>
2020-01-15 20:20:10 +0100

    Show subparts with missing parent

--- tb_svelte/src/Subparts.svelte
+++ tb_svelte/src/Subparts.svelte
@@ -1,5 +1,5 @@
 <script>
-import {types} from './store.js'
+import {types, filterValues} from './store.js'
 import Usage from './Usage.svelte'
 import NewPart from './NewPart.svelte'
 
@@ -9,41 +9,49 @@ export let level = undefined;
 
 let part
 
+let hooks = filterValues($types, (a) => a.hooks.includes(hook)).sort((a,b) => a.order - b.order)
 </script>
 {#if subparts.length > 0}
-{#if level === undefined}
-  <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Part</th>
-      <th scope="col">Name</th>
-      <th scope="col" class="text-right">Attached</th>
-      <Usage />
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <svelte:self {hook} {subparts} level={0}></svelte:self>
-  </tbody>
-  </table>
-  
-{:else}
-
-  {#each subparts
-      .sort((a,b) => $types[a.what].order > $types[b.what].order)
-      .filter((a) => a.hook == hook)
-    as {what, name, part_id, attached} (part_id)}
-    <tr>
-      <th scope="row" class="text-nowrap"> {'| '.repeat(level)+$types[what].name} </th>
-      <td>{name}</td>
-      <td class="text-right"> {new Date(attached).toLocaleDateString()} </td >
-      <Usage {part_id} />
-      <th> <NewPart title='New' cat={$types[what]}/></th>
-    </tr>
-    <svelte:self hook={what} {subparts} level={level+1}></svelte:self>
-  {/each}
-
-{/if}
+  {#if level === undefined}
+    <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">Part</th>
+        <th scope="col">Name</th>
+        <th scope="col" class="text-right">Attached</th>
+        <Usage />
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <svelte:self {hook} {subparts} level={0}></svelte:self>
+    </tbody>
+    </table>
+    
+  {:else}
+    {#each hooks as type}
+      {#each subparts.filter((a) => a.what == type.id && a.hook == hook)
+        as {what, name, part_id, attached} (part_id)}
+        <slot />  
+        <tr>
+          <th scope="row" class="text-nowrap"> {'| '.repeat(level)+type.name} </th>
+          <td>{name}</td>
+          <td class="text-right"> {new Date(attached).toLocaleDateString()} </td >
+          <Usage {part_id} />
+          <th> <NewPart title='New' cat={type}/></th>
+        </tr>
+        
+        <svelte:self hook={type.id} {subparts} level={level+1}></svelte:self>
+      {:else}
+        <svelte:self hook={type.id} {subparts} level={level+1}>
+          <tr>
+            <th scope="row" class="text-nowrap"> {'| '.repeat(level)+type.name} </th>
+          </tr>
+        </svelte:self>
+      
+      {/each}
+    {/each}
+  {/if}
 {:else}
    No subparts maintained!
 {/if}
