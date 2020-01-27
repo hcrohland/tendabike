b04e07f8c896092e32407b4631b515e1bc8b34c0
Christoph Rohland <hcrohland@users.noreply.github.com>
2020-01-27 14:57:52 +0100

    Calculate spares from attachments

--- tb_svelte/src/Spares.svelte
+++ tb_svelte/src/Spares.svelte
@@ -1,28 +1,29 @@
 <script>
-import {myfetch, handleError, filterValues, types, parts, category} from './store.js'
+import {filterValues, types, parts, attachments, category} from './store.js'
 import Usage from './Usage.svelte'
 import Await from './Await.svelte'
 import Attach from './Attach.svelte'
 import NewPart from './NewPart.svelte'
 
 export let params;
-  
+export let date = new Date;
+
 // Cannot use category directly since it 
 // is unset during destroy and the router gets confused
 let cat = $types[params.category]
 category.set(cat);
 
-let spares
+let spareTypes = filterValues($types, (t) => t.main == cat.id && t.id != cat.id)
 
-let promise; 
-
-update();
-
-function update() {
-  console.log("update")
-  promise = myfetch('/part/spares/' + cat.id).then((d) => spares = d)
+function attachedTo(atts, partId, time) {
+    let att = filterValues(atts, (x) => x.part_id === partId && time >= new Date(x.attached) && (!x.detached || time < new Date(x.detached) ))
+    return att.pop()
 }
 
+function subparts(type, parts) {
+  return filterValues(parts, (p) => p.what == type.id)
+            .sort((a,b) => a.purchase < b.purchase)
+}
 </script>
 
 <style>
@@ -31,39 +32,29 @@ function update() {
  }
 </style>
 
-
-{#await promise}
-	<Await />
-{:then}
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Part</th>
-          <th scope="col">Name</th>
-          <Usage />
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-      {#each filterValues($types, (t) => t.main == cat.id && t.id != cat.id) as type}
-        <tr>
-            <th colspan=8 scope="col" class="border-2 text-nowrap"> {type.name}s <NewPart title='New' cat={type} on:created={update}/></th>
-        </tr>
-          
-         {#each spares
-            .map((s) => $parts[s])
-            .filter((p) => p.what == type.id).sort((a,b) => a.purchase < b.purchase)
-          as part (part.id)}
-          <tr>
-           <td class="border-0"></td>
-            <td title={part.vendor + ' ' + part.model + ' ' + new Date(part.purchase).toLocaleDateString()}>{part.name}</td>
-            <Usage part_id={part.id} />
-            <td> <Attach {part} on:saved={update}/></td>
-          </tr>
-        {/each}
-      {/each}
-      </tbody>
-    </table>
-{:catch error}
-  {handleError(error)}
-{/await}
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Part</th>
+      <th scope="col">Name</th>
+      <Usage />
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+  {#each spareTypes as type (type.id)}
+    <tr>
+        <th colspan=8 scope="col" class="border-2 text-nowrap"> {type.name}s <NewPart title='New' cat={type}/></th>
+    </tr>
+      {#each subparts(type, $parts).filter((p) => !attachedTo($attachments, p.id, date))
+      as part (part.id)}
+      <tr>
+        <td class="border-0"></td>
+        <td title={part.vendor + ' ' + part.model + ' ' + new Date(part.purchase).toLocaleDateString()}>{part.name}</td>
+        <Usage part_id={part.id} />
+        <td> <Attach {part}/></td>
+      </tr>
+    {/each}
+  {/each}
+  </tbody>
+</table>
