42311856666025463971ed286bdf179ac9c4fd7d
Christoph Rohland <hcrohland@users.noreply.github.com>
2020-11-30 11:33:18 +0100

    Startup message

--- server/src/bin/tendabike.rs
+++ server/src/bin/tendabike.rs
@@ -6,5 +6,9 @@ fn main() {
     init_environment();
 
     // start the server
-    ignite_rocket().launch();
+    ignite_rocket()
+    .attach(rocket::fairing::AdHoc::on_launch("Launch Message", |rocket| {
+        let c = rocket.config();
+        eprintln!("\nInfo: TendaBike running on {}:{}\n", c.address, c.port);
+    })).launch();
 }
