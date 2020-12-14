49e93ef0af3f92f97ce8cc72e0b7a8faeac38a84
Christoph Rohland <hcrohland@users.noreply.github.com>
2020-12-14 11:53:15 +0100

    Add Plotly

---
+++ frontend/src/Widgets/Plotly.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from 'svelte';
+  export let data;
+  export let layout = undefined;
+  export let config = undefined;
+  
+  let redraw = (a,b,c) => {};
+
+  let div = 'plotly_div_' + Math.random()
+
+  onMount(() => {
+  
+      let plotDiv = document.getElementById(div)
+
+      Plotly.newPlot(plotDiv, data, layout, config); 
+      redraw = (d, l, c) => Plotly.react(plotDiv,d, l, c) 
+  });
+  
+  $: redraw(data, layout, config)
+</script>
+
+  <div id={div}><!-- Plotly chart will be drawn inside this DIV --></div>
