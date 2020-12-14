3e47d3739699e6816b194c67f487caff9444d934
Christoph Rohland <hcrohland@users.noreply.github.com>
2020-12-14 15:21:25 +0100

    Title for Plotly

--- frontend/src/Widgets/Plotly.svelte
+++ frontend/src/Widgets/Plotly.svelte
@@ -3,17 +3,19 @@
   export let data;
   export let layout = undefined;
   export let config = undefined;
-  
+  export let title
+
   let redraw = (a,b,c) => {};
 
-  let div = 'plotly_div_' + Math.random()
+  let div = 'plotly_div_' + title
 
+  layout = Object.assign(Object.assign({}, layout), {title: {text: title}})
   onMount(() => {
   
       let plotDiv = document.getElementById(div)
 
       Plotly.newPlot(plotDiv, data, layout, config); 
-      redraw = (d, l, c) => Plotly.react(plotDiv,d, l, c) 
+      redraw = (d, l, c) => Plotly.newPlot(plotDiv,d, l, c) 
   });
   
   $: redraw(data, layout, config)
