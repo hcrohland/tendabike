00f7e8bb8e320cfee2a796656620a3808fbbd234
Christoph Rohland <hcrohland@users.noreply.github.com>
2020-12-20 10:04:24 +0100

    Simplify Plotly

--- frontend/src/Widgets/Plotly.svelte
+++ frontend/src/Widgets/Plotly.svelte
@@ -7,18 +7,15 @@
 
   let redraw = (a,b,c) => {};
 
-  let div = 'plotly_div_' + title
+  let plotDiv
 
-  layout = Object.assign(Object.assign({}, layout), {title: {text: title}})
+  layout = Object.assign({}, layout, {title: {text: title}})
+
   onMount(() => {
-  
-      let plotDiv = document.getElementById(div)
-
-      Plotly.newPlot(plotDiv, data, layout, config); 
       redraw = (d, l, c) => Plotly.newPlot(plotDiv,d, l, c) 
   });
   
   $: redraw(data, layout, config)
 </script>
 
-  <div id={div}><!-- Plotly chart will be drawn inside this DIV --></div>
+  <div bind:this={plotDiv}><!-- Plotly chart will be drawn inside this DIV --></div>
