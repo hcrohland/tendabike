720f16287ee840c2c0323b2c7b6feddfd71895ce
Christoph Rohland <hcrohland@users.noreply.github.com>
2023-05-14 18:24:33 +0200

    First step to pool established by s_diesel

--- server/kernel/src/s_diesel/appconn.rs
+++ server/kernel/src/s_diesel/appconn.rs
@@ -1,11 +1,13 @@
 use std::ops::Deref;
 
+use crate::domain::AnyResult;
+
 use super::schema;
 use diesel::prelude::*;
 use chrono::{DateTime, Utc};
 
 pub type AppConn = PgConnection;
-pub struct Store (diesel::PgConnection);
+pub struct Store (PooledConnection<ConnectionManager<PgConnection>>);
 
 impl Deref for Store {
     type Target = diesel::PgConnection;
@@ -16,8 +18,9 @@ impl Deref for Store {
 }
 
 impl Store {
-    pub fn new(conn: PgConnection) -> Self{
-        Self(conn)
+    pub fn new(pool: DbPool) -> AnyResult<Self> {
+        let conn = pool.get()?;
+        Ok(Store(conn))
     }
 }
 
@@ -34,4 +37,23 @@ pub fn run_db_migrations (conn: &AppConn) {
 
     embedded_migrations::run(conn)
         .expect("Failed to run database migrations: {:?}");
+}
+
+
+use r2d2::{Pool, PooledConnection};
+use diesel::pg::PgConnection;
+use diesel::r2d2::ConnectionManager;
+
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+pub fn get_connection_pool() -> DbPool {
+    let database_url = std::env::var("DB_URL").unwrap_or(
+        "postgres://localhost/tendabike".to_string());
+        
+    println!("Connecting to database {}...", database_url);
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    Pool::builder()
+        .build(manager)
+        .expect("Failed to create database connection pool.")
 }
