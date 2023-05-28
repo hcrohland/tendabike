528a2d3cd765630229218f92b3f7c3e4438ba7e6
Christoph Rohland <hcrohland@users.noreply.github.com>
2023-05-28 17:04:58 +0200

    Add License annotations and github link (#37)

--- server/kernel/src/domain/user.rs
+++ server/kernel/src/domain/user.rs
@@ -1,4 +1,24 @@
-use super::*;
+/* 
+    tendabike - the bike maintenance tracker
+    
+    Copyright (C) 2023  Christoph Rohland 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
+
+ use super::*;
 use schema::users;
 
 #[derive(DieselNewType, Clone, Copy, Debug, Default, Hash, PartialEq, Eq, Serialize, Deserialize)]
