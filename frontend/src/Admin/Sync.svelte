96af71c25ceb05f2f939c9332aac64138a017487
Christoph Rohland <hcrohland@users.noreply.github.com>
2023-06-16 07:52:40 +0200

    Auto refresh and stable user list in Admin

--- frontend/src/Admin/Sync.svelte
+++ frontend/src/Admin/Sync.svelte
@@ -1,31 +1,33 @@
-<script lang="ts" >
-import { Button } from 'sveltestrap';
-import {handleError, myfetch} from '../store'
-import type {User} from '../types'
+<script lang="ts">
+  import { Button } from "sveltestrap";
+  import { handleError, myfetch } from "../store";
+  import type { User } from "../types";
 
-export let user: User;
+  export let user: User;
+  export let refresh: () => void;
 
-let promise, count
+  let promise, count;
 
-const sync = (id) => {promise = getdata(id)}
-async function getdata(id) {
-    let data
+  const sync = (id) => {
+    promise = getdata(id);
+  };
+  async function getdata(id) {
+    let data;
     count = 0;
     do {
-      data = await myfetch('/strava/sync/' + id).catch(handleError)
+      data = await myfetch("/strava/sync/" + id).catch(handleError);
       if (!data) break;
       count += data["activities"].length;
-    } while (data["activities"].length > 0)
+    } while (data["activities"].length > 0);
     count = 0;
+    refresh();
   }
-
 </script>
 
-<Button on:click={() => sync(user.id)}> 
+<Button on:click={() => sync(user.id)}>
   {#await promise}
     Processed {count} ...
   {:then}
     Process Event Queue
-  {/await} 
+  {/await}
 </Button>
-  
