62d489405c415ecda5d1a2497b4929352ec7faa7
Christoph Rohland <hcrohland@users.noreply.github.com>
2024-01-05 20:03:40 +0100

    add time range select
    * add time range select

--- frontend/src/Activities/ActTable.svelte
+++ frontend/src/Activities/ActTable.svelte
@@ -1,105 +1,161 @@
 <script lang="ts">
-  import {category, activities, parts, filterValues, fmtNumber, fmtSeconds} from '../store'
-  import SvelteTable from "../Widgets/SvelteTable.svelte";
-  import {addToUsage, newUsage, type Activity, type Part} from "../types"
+	import {
+		by,
+		category,
+		activities,
+		parts,
+		filterValues,
+		fmtNumber,
+		fmtSeconds,
+	} from "../store";
+	import SvelteTable from "../Widgets/SvelteTable.svelte";
+	import { addToUsage, newUsage, type Activity, type Part } from "../types";
+	import RangeSlider from "svelte-range-slider-pips";
 
-  export let rows: Activity[] = filterValues($activities, (a) =>  $category.acts.some((t) => t.id == a.what))
+	export let acts: Activity[] = filterValues($activities, (a) =>
+		$category ? $category.acts.some((t) => t.id == a.what) : false,
+	).sort(by("start"));
 
-  const gearname = v => {let x = $parts[v.gear]; if (x) {return x.name }else{return "-"}};
+	const DAY = 24 * 3600000;
 
-  let columns = [
-    {
+	let max = Math.floor(acts[0].start.getTime() / DAY);
+	let min = Math.floor(acts[acts.length - 1].start.getTime() / DAY);
+	let values = [min, max];
+
+	$: rows = filterRows(acts, values);
+
+	function filterRows(acts: Activity[], values: Number[]) {
+		let start = new Date(values[0] * DAY);
+		let end = new Date(values[1] * DAY);
+
+		return acts.filter((a) => {
+			let starttime = a.start.getTime();
+			return start.getTime() <= starttime && end.getTime() + DAY > starttime;
+		});
+	}
+
+	const formatter = (v) => new Date(v * DAY).toLocaleDateString();
+
+	const gearname = (v) => {
+		let x = $parts[v.gear];
+		if (x) {
+			return x.name;
+		} else {
+			return "-";
+		}
+	};
+
+	let columns = [
+		{
 			key: "start",
 			title: "Start",
-			value: v => v.start,
+			value: (v) => v.start,
 			sortable: true,
-      searchValue: v => v.start.toLocaleDateString() + ' ' + v.start.toLocaleTimeString(),
-      renderValue: v => v.start ? v.start.toLocaleDateString() + ' ' + v.start.toLocaleTimeString() : '',
+			searchValue: (v) =>
+				v.start.toLocaleDateString() + " " + v.start.toLocaleTimeString(),
+			renderValue: (v) =>
+				v.start
+					? v.start.toLocaleDateString() + " " + v.start.toLocaleTimeString()
+					: "",
 		},
 		{
 			key: "name",
 			title: "Name",
-			value: v => v.name || '',
-			searchValue: v => v.name,
+			value: (v) => v.name || "",
+			searchValue: (v) => v.name,
 			sortable: true,
 		},
-    {
+		{
 			key: "gear",
 			title: "Gear",
 			value: gearname,
 			sortable: true,
-			filterValue: v => v.gear,
-      filterOptions: rows => {
+			filterValue: (v) => v.gear,
+			filterOptions: (rows) => {
 				let types = {};
-				rows.forEach(row => {
+				rows.forEach((row) => {
 					let name = gearname(row);
 					if (types[row.gear] === undefined)
-						types[row.gear] = {name: name, value: row.gear};
+						types[row.gear] = { name: name, value: row.gear };
 				});
 				// fix order
 				types = Object.entries(types)
 					.sort()
 					.reduce((o, [k, v]) => ((o[k] = v), o), {});
 				return Object.values(types);
-			},},
-    {
+			},
+		},
+		{
 			key: "climb",
 			title: "Climb",
-			value: v => fmtNumber(v.climb),
+			value: (v) => fmtNumber(v.climb),
 			sortable: true,
 			class: "text-end",
 		},
-    {
+		{
 			key: "descend",
 			title: "Descend",
-			value: v => fmtNumber(v.descend || v.climb),
+			value: (v) => fmtNumber(v.descend || v.climb),
 			sortable: true,
 			class: "text-end",
 		},
-    {
+		{
 			key: "distance",
 			title: "Distance",
-			value: v => fmtNumber(v.distance),
+			value: (v) => fmtNumber(v.distance),
 			sortable: true,
 			class: "text-end",
 		},
-    {
+		{
 			key: "time",
 			title: "Time",
-			value: v => fmtSeconds(v.time),
+			value: (v) => fmtSeconds(v.time),
 			sortable: true,
 			class: "text-end",
 		},
-    {
+		{
 			key: "duration",
 			title: "Duration",
-			value: v => fmtSeconds(v.duration),
+			value: (v) => fmtSeconds(v.duration),
 			sortable: true,
 			class: "text-end",
-		}
-  ];
+		},
+	];
 
 	const totalsFunc = (r: Activity[]) => {
-			return r.reduce((total, row) => {
+		return r.reduce(
+			(total, row) => {
 				addToUsage(total, row);
 				return total;
-			}, 
+			},
 			{
 				name: "Totals:",
-				... newUsage()
-			}
-		)
-	}
+				...newUsage(),
+			},
+		);
+	};
 </script>
 
-<SvelteTable 
-  {columns} 
-  {rows} 
-  sortOrders={[-1,1]} 
-  sortBy=start 
-	{totalsFunc}
-  classNameTable={['table']}
-  classNameThead={['table-secondary']}
-  classNameSelect={['custom-select']} 
-/>
+<RangeSlider
+	{min}
+	{max}
+	range
+	pushy
+	pips
+	first="label"
+	last="label"
+	float
+	{formatter}
+	bind:values
+></RangeSlider>
 
+<SvelteTable
+	{columns}
+	{rows}
+	sortOrders={[-1, 1]}
+	sortBy="start"
+	{totalsFunc}
+	classNameTable={["table"]}
+	classNameThead={["table-secondary"]}
+	classNameSelect={["custom-select"]}
+/>
