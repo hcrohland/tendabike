c4694be7eb9b4f887f3f60911d82c655557c5d3b
Christoph Rohland <hcrohland@users.noreply.github.com>
2024-01-16 14:03:56 +0100

    fix Garmin.svelte formatting

--- frontend/src/Actions/Garmin.svelte
+++ frontend/src/Actions/Garmin.svelte
@@ -1,51 +1,56 @@
 <script lang="ts">
   import {
-    Modal, ModalBody, ModalHeader,
-    ListGroup, ListGroupItem, Input,
-    InputGroup, InputGroupText, Label
-  } from '@sveltestrap/sveltestrap';
-  import {attachments, checkStatus, handleError, parts} from "../lib/store";
-  import TZPicker from '../Widgets/TZPicker.svelte';
-  import ModalFooter from './ModalFooter.svelte'
+    Modal,
+    ModalBody,
+    ModalHeader,
+    ListGroup,
+    ListGroupItem,
+    Input,
+    InputGroup,
+    InputGroupText,
+    Label,
+  } from "@sveltestrap/sveltestrap";
+  import { attachments, checkStatus, handleError, parts } from "../lib/store";
+  import TZPicker from "../Widgets/TZPicker.svelte";
+  import ModalFooter from "./ModalFooter.svelte";
 
-  let timezone: string| undefined;
+  let timezone: string | undefined;
 
   let isOpen = false;
   let files: FileList | undefined;
-  let result: {good:string[], bad:string[]} | undefined;
+  let result: { good: string[]; bad: string[] } | undefined;
   let button: string | undefined;
-  const toggle = () => isOpen = false
+  const toggle = () => (isOpen = false);
 
   export const garmin = () => {
-    files = undefined; 
+    files = undefined;
     timezone = undefined;
     result = undefined;
     button = "Synchronize";
     isOpen = true;
-  }
-
-  $: disabled = !(files && files[0])
-
-  async function sendFile () {
-    var body = files && await files[0].text();
-    return fetch('/activ/descend?tz=' + timezone, {
-            method: 'POST',
-            credentials: 'include',
-            body
-        })
-        .then(checkStatus)
-        .then((a) => {
-          parts.updateMap(a[0].parts); 
-          attachments.updateMap(a[0].attachments);
-          result = {
-            good: a[1],
-            bad: a[2]
-          }
-          button = undefined
-        }
-        )
-        .catch(handleError)
   };
+
+  $: disabled = !(files && files[0]);
+
+  async function sendFile() {
+    var body = files && (await files[0].text());
+    return fetch("/activ/descend?tz=" + timezone, {
+      method: "POST",
+      credentials: "include",
+      body,
+    })
+      .then(checkStatus)
+      .then((a) => {
+        parts.updateMap(a[0].parts);
+        attachments.updateMap(a[0].attachments);
+        result = {
+          good: a[1],
+          bad: a[2],
+        };
+        button = undefined;
+      })
+      .catch(handleError);
+  }
 </script>
 
 <Modal {isOpen} {toggle} backdrop={false} scrollable>
@@ -53,34 +58,34 @@
   {#if result}
     <ModalBody>
       {#if result.good.length > 0}
-        Synchronized {result.good.length} activities. 
+        Synchronized {result.good.length} activities.
       {/if}
       {#if result.bad.length > 0}
-        <br><br>Could not match the following {result.bad.length} activities:
+        <br /><br />Could not match the following {result.bad.length} activities:
         <ListGroup>
           {#each result.bad as r}
-          <ListGroupItem>{r}</ListGroupItem>
+            <ListGroupItem>{r}</ListGroupItem>
           {/each}
         </ListGroup>
       {/if}
     </ModalBody>
   {:else}
-    <ModalBody>      
-      <Input 
-        type="file" 
-        bind:files 
-        accept="text/csv" 
+    <ModalBody>
+      <Input
+        type="file"
+        bind:files
+        accept="text/csv"
         title="Upload a CSV file exported from Garmin connect activities. 
 It will match activities based on the start time. 
 If there is no match it will skip the activity - maybe it was in another timezone? 
-You can upload multiple times" 
+You can upload multiple times"
       />
-      <br>
+      <br />
       <InputGroup class="mb-0 mr-sm-2 mb-sm-2">
-        <InputGroupText>Timezone of activities: </InputGroupText>
+        <InputGroupText>Timezone of activities:</InputGroupText>
         <TZPicker bind:timezone />
       </InputGroup>
-      </ModalBody>
+    </ModalBody>
   {/if}
-  <ModalFooter {toggle} {disabled} action={sendFile} {button}/>
+  <ModalFooter {toggle} {disabled} action={sendFile} {button} />
 </Modal>
