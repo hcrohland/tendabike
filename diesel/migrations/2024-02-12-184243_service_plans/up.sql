08fc17e1c566058cc6f67da61a3d7d426dfeb7b8
Christoph Rohland <hcrohland@users.noreply.github.com>
2024-02-12 19:50:44 +0100

    create table service_plans

---
+++ diesel/migrations/2024-02-12-184243_service_plans/up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE service_plans (
+    "id" uuid PRIMARY KEY,
+    "part" integer NOT NULL,
+    "what" integer NOT NULL,
+    "hook" integer,
+    "name" text NOT NULL,
+    "days" integer,
+    "time" integer,
+    "distance" integer,
+    "climb" integer,
+    "descend" integer,
+    "count" integer
+);
