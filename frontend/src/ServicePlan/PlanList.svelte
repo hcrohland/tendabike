aeaed7ae3702bed72fe84c797228045fb4ff2dff
Christoph Rohland <hcrohland@users.noreply.github.com>
2024-04-22 10:26:17 +0200

    Better sort function

--- frontend/src/ServicePlan/PlanList.svelte
+++ frontend/src/ServicePlan/PlanList.svelte
@@ -2,10 +2,23 @@
   import { Table } from "@sveltestrap/sveltestrap";
   import { ServicePlan } from "../lib/serviceplan";
   import PlanHeader from "./PlanHeader.svelte";
-  import { by } from "../lib/mapable";
   import PlanBlock from "./PlanBlock.svelte";
 
   export let planlist: ServicePlan[];
+
+  function cmp(p: ServicePlan, q: ServicePlan) {
+    let res;
+    if (p.what != q.what) {
+      res = p.what < q.what;
+    } else if (p.hook != q.hook) {
+      res = p.what < q.what;
+    } else if (p.part != q.part) {
+      res = p.part! < q.part!;
+    } else {
+      res = p.id! < q.id!;
+    }
+    return res ? -1 : 1;
+  }
 </script>
 
 <Table responsive hover>
@@ -13,10 +26,7 @@
     <PlanHeader />
   </thead>
   <tbody>
-    {#each planlist
-      .sort(by("id"))
-      .sort(by("part"))
-      .sort(by("what", true)) as plan}
+    {#each planlist.sort(cmp) as plan}
       <PlanBlock {plan} />
     {/each}
   </tbody>
