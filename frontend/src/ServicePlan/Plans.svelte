22c328161a386caf03c03b0fdaa1aeacadecbd78
Christoph Rohland <hcrohland@users.noreply.github.com>
2024-04-23 11:21:16 +0200

    only show plans for current  category

--- frontend/src/ServicePlan/Plans.svelte
+++ frontend/src/ServicePlan/Plans.svelte
@@ -3,11 +3,15 @@
   import { plans } from "../lib/serviceplan";
   import PlanList from "./PlanList.svelte";
   import NewPlan from "./NewPlan.svelte";
+  import { filterValues } from "../lib/mapable";
+  import { category } from "../lib/types";
 
   let newPlan: () => void;
+
+  $: planlist = filterValues($plans, (p) => p.what == $category.main);
 </script>
 
-<PlanList planlist={Object.values($plans)}>
+<PlanList {planlist}>
   Serviceplans
   <Button size="sm" color="light" on:click={() => newPlan()}>add</Button> &NonBreakingSpace;
 </PlanList>
