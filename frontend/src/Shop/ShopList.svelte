1db557045eee1515ff0ab78a58895ba9aa8db8c0
Christoph Rohland <hcrohland@users.noreply.github.com>
2026-01-12 19:03:23 +0000

    Add registration link generation

--- frontend/src/Shop/ShopList.svelte
+++ frontend/src/Shop/ShopList.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
-  import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+  import {
+    Button,
+    Clipboard,
+    Dropdown,
+    DropdownItem,
+    Input,
+    Tooltip,
+  } from "flowbite-svelte";
   import ShopCard from "./ShopCard.svelte";
   import { type Shop } from "../lib/shop";
   import { actions } from "../Widgets/Actions.svelte";
   import { user } from "../lib/store";
   import type { Snippet } from "svelte";
-  import { DotsVerticalOutline } from "flowbite-svelte-icons";
+  import {
+    CheckOutline,
+    ClipboardCleanSolid,
+    DotsVerticalOutline,
+  } from "flowbite-svelte-icons";
 
   interface Props {
     shops: Shop[];
@@ -28,6 +39,28 @@
           <DropdownItem onclick={() => $actions.deleteShop(shop)}>
             Delete Shop
           </DropdownItem>
+          <DropdownItem>Registration Link</DropdownItem>
+          <Dropdown simple>
+            <DropdownItem>
+              {@const value = window.location.origin + "/#/register/" + shop.id}
+              <Input {value} readonly>
+                {#snippet right()}
+                  <Clipboard {value} embedded>
+                    {#snippet children(success)}
+                      <Tooltip isOpen={success}>
+                        {success ? "Copied" : "Copy to clipboard"}
+                      </Tooltip>
+                      {#if success}
+                        <CheckOutline />
+                      {:else}
+                        <ClipboardCleanSolid />
+                      {/if}
+                    {/snippet}
+                  </Clipboard>
+                {/snippet}
+              </Input>
+            </DropdownItem>
+          </Dropdown>
         </Dropdown>
       {:else}
         <Button onclick={() => $actions.requestSubscription(shop)}>
