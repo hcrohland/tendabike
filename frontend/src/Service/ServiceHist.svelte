13b16bc62027ffddfd92f35961d737550aa6d114
Christoph Rohland <hcrohland@users.noreply.github.com>
2026-06-30 14:14:51 +0000

    Responsive margins

--- frontend/src/Service/ServiceHist.svelte
+++ frontend/src/Service/ServiceHist.svelte
@@ -16,12 +16,12 @@
   let successor = $derived(service.get_successor($services));
 </script>
 
-<div class="rounded-lg border border-border-subtle bg-surface-1 p-3">
+<div class="rounded-lg border border-border-subtle bg-surface-1 p-2">
   <ServiceRow {part} {service} {successor}>
     <ShowMore bind:show_more title="history" />
   </ServiceRow>
   {#if show_more}
-    <div class="rounded-lg border border-border-subtle bg-surface-1 p-3">
+    <div class="rounded-lg border border-border-subtle bg-surface-1 p-2">
       <div class="flex flex-col gap-2 mt-2">
         {#each service.history(1, $services) as s (s.service?.id + "-" + s.successor?.id)}
           <ServiceRow {part} {...s} />
